62c3db3fa137df1ab2e823560109038eac707664
Yue Dong <ydong011@fiu.edu>
2022-08-31 22:45:09 -0500

    update

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,16 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+import('./bootstrap').catch((err) => console.error(err));
+
+/* 
+  
+    & create Monorepo
+    $ ng new microfrontend-demo --create-application=false
+
+    & crate shell and mfe1
+    $ ng g application shell
+    $ ng g application mfe1
+
+    & add @angular-architects/module-federation
+    $ ng add @angular-architects/module-federation --project shell --type host --port 4200
+    $ ng add @angular-architects/module-federation --project mfe1 --type remote --port 4201
+    
+  */
