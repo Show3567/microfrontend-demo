eeb9788da071d74687b078ac66a38f547ff97ebc
Yue Dong <ydong011@fiu.edu>
2022-08-31 23:15:17 -0500

    update

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'flights',
-    loadChildren: () => import('mfe1/Module').then((m) => m.FlightsModule),
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'mfe1',
+        exposedModule: './Module',
+      }).then((m) => m.FlightsModule),
   },
 ];
 
