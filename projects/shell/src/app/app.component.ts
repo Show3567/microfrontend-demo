8fd63265cbc60474bb83c19185afcef4ab0379b0
Yue Dong <ydong011@fiu.edu>
2022-09-02 13:43:08 -0500

    update

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ShellService } from './services/shell.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'shell';
+
+  constructor(public shellService: ShellService) {}
 }
