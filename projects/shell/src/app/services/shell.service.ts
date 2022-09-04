7743996b23fb2a9d14d8d5d427bbe81d205df11b
Yue Dong <ydong011@fiu.edu>
2022-09-04 09:32:35 -0500

    update

--- projects/shell/src/app/services/shell.service.ts
+++ projects/shell/src/app/services/shell.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, fromEvent, Observable, Subscription } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +9,36 @@ export class ShellService {
   private counter = 1;
   counter$ = new BehaviorSubject<number>(this.counter);
 
-  constructor() {}
+  // sessionListener$: Observable<StorageEvent> = fromEvent<StorageEvent>(
+  //   window,
+  //   'storage'
+  // ).pipe(
+  //   filter((event) => event.storageArea === sessionStorage),
+  //   filter((event) => event.key === 'message'),
+  //   tap((msg) => {
+  //     console.log('msg: ', msg);
+  //   })
+  // );
+
+  constructor() {
+    fromEvent<StorageEvent>(window, 'storage').pipe(
+      filter((event) => event.storageArea === sessionStorage),
+      filter((event) => event.key === 'message'),
+      tap((msg) => {
+        console.log('msg: ', msg);
+      })
+    );
+  }
 
   increase() {
     this.counter++;
+    sessionStorage.setItem('counter', this.counter.toString());
     this.counter$.next(this.counter);
   }
 
   decrease() {
     this.counter--;
+    sessionStorage.setItem('counter', this.counter.toString());
     this.counter$.next(this.counter);
   }
 }
