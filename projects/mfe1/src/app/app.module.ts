6e8f7d624ce6ae63de0a075474a8fe75f8017717
Yue Dong <ydong011@fiu.edu>
2023-06-14 12:21:46 -0500

    update

--- projects/mfe1/src/app/app.module.ts
+++ projects/mfe1/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CounterModule } from './counter/counter.module';
     BrowserModule,
     AppRoutingModule,
     FlightsModule,
+
     StoreModule.forRoot({}, {}),
     CounterModule,
   ],
